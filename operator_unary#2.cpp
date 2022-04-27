0a3c077268901803e5ffb42c330860ac8d2b540a
Salim28 <103082694+Salim28@users.noreply.github.com>
2022-04-26 18:20:04 -0700

    Create operator_unary#2.cpp

---
+++ operator_unary#2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+ float a=sqrt(25);//menghitung akar kuadrat
+ float b=cbrt(27);//menghitung akar kubik
+ float c=pow(4,2);//menghitung pangkat
+ //tambahkan dan tampilkan d=6 e=8 f=tan
+ float d=sin(3);//menghitung sin
+ float e=cos(50);//menghitung cos
+ float f=tan(180);//menghitung tan
+ float g=round(d);//menghitung membulatkan
+ cout<<a<<endl;
+ cout<<b<<endl;
+ cout<<c<<endl;
+ cout<<d<<endl;
+ cout<<e<<endl;
+ cout<<f<<endl;
+ cout<<g<<endl;
+ return 0;
+}
